54f0ef8647a615b95def1d307ce9d9f69f74311f
iloktionov <iloktionov@skbkontur.ru>
2017-08-21 15:53:24 +0500

    ContextConfiguration: got rid of ImmutableHashSet.

--- Vostok.Core/Flow/ContextConfiguration.cs
+++ Vostok.Core/Flow/ContextConfiguration.cs
@@ -1,12 +1,12 @@
 using System.Collections.Generic;
-using System.Collections.Immutable;
+using System.Threading;
 
 namespace Vostok.Flow
 {
     internal class ContextConfiguration : IContextConfiguration
     {
-        private ImmutableHashSet<string> distributedProperties = ImmutableHashSet<string>.Empty;
         private readonly object syncObject = new object();
+        private HashSet<string> distributedProperties = new HashSet<string>();
 
         public bool IsDistributedProperty(string key)
         {
@@ -17,7 +17,15 @@ namespace Vostok.Flow
         {
             lock (syncObject)
             {
-                distributedProperties = distributedProperties.Add(key);
+                if (distributedProperties.Contains(key))
+                    return;
+
+                var newProperties = new HashSet<string>(distributedProperties)
+                {
+                    key
+                };
+
+                Interlocked.Exchange(ref distributedProperties, newProperties);
             }
         }
     }
