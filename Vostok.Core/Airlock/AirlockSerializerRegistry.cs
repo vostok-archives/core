5558544b6b7177234a809a06f00d578a0d4b6014
iloktionov <iloktionov@skbkontur.ru>
2017-08-22 12:37:09 +0500

    Implemented AirlockSerializerRegistry.

--- Vostok.Core/Airlock/AirlockSerializerRegistry.cs
+++ Vostok.Core/Airlock/AirlockSerializerRegistry.cs
@@ -1,10 +1,22 @@
-﻿namespace Vostok.Airlock
+﻿using System.Threading;
+
+namespace Vostok.Airlock
 {
     public static class AirlockSerializerRegistry
     {
         public static void Register<T>(IAirlockSerializer<T> serializer)
         {
-            // ...
+            Interlocked.Exchange(ref Container<T>.Serializer, serializer);
+        }
+
+        public static bool TryGet<T>(out IAirlockSerializer<T> serializer)
+        {
+            return (serializer = Container<T>.Serializer) != null;
+        }
+
+        private static class Container<T>
+        {
+            public static IAirlockSerializer<T> Serializer;
         }
     }
 }
