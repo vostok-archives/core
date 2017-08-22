2899bd9e5b1335f8780c629176890a7f89df4bd8
iloktionov <iloktionov@skbkontur.ru>
2017-08-22 16:40:00 +0500

    Tracing configuration now contains an airlock client implementation.

--- Vostok.Core/Tracing/ITraceConfiguration.cs
+++ Vostok.Core/Tracing/ITraceConfiguration.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using Vostok.Airlock;
 
 namespace Vostok.Tracing
 {
     public interface ITraceConfiguration
     {
         ISet<string> ContextFieldsWhitelist { get; }
+
+        IAirlock Airlock { get; set; }
     }
 }
--- Vostok.Core/Tracing/TraceConfiguration.cs
+++ Vostok.Core/Tracing/TraceConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Vostok.Airlock;
 using Vostok.Commons.Collections;
 
 namespace Vostok.Tracing
@@ -7,5 +8,7 @@ namespace Vostok.Tracing
     internal class TraceConfiguration : ITraceConfiguration
     {
         public ISet<string> ContextFieldsWhitelist { get; } = new ConcurrentSet<string>(StringComparer.Ordinal);
+
+        public IAirlock Airlock { get; set; }
     }
 }
