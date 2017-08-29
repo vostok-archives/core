f8f3acb072cdb06b2dcd7c0fe149f7573a96fd97
iloktionov <iloktionov@skbkontur.ru>
2017-08-29 18:09:35 +0500

    Added a trivial dev/null implementation of ILog.

---
+++ Vostok.Core/Logging/SilentLog.cs
@@ -0,0 +1,14 @@
+﻿namespace Vostok.Logging
+{
+    public class SilentLog : ILog
+    {
+        public void Log(LogEvent logEvent)
+        {
+        }
+
+        public bool IsEnabledFor(LogLevel level)
+        {
+            return false;
+        }
+    }
+}
