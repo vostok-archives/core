8a3c1940d5d498ecc55ca54444609d25dc141102
johneg <johneg@skbkontur.ru>
2017-09-15 11:38:14 +0500

    Поправил баги и добавил тестов

--- Vostok.Core/Flow/DistributedContextSerializer/CharSerializer.cs
+++ Vostok.Core/Flow/DistributedContextSerializer/CharSerializer.cs
@@ -2,6 +2,8 @@
 {
     public class CharSerializer : BaseTypedSerializer<char>
     {
+        public override string Id => "char";
+
         protected override bool TryDeserialize(string serializedValue, out char value)
             => char.TryParse(serializedValue, out value);
     }
