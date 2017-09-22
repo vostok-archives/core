f71462c8ba5f21b77f05fb557b8b44f0fd64cc2c
Andrey Lapin <lapin@kontur.ru>
2017-09-22 18:08:16 +0400

    added ILog extensions for key-value and custom class

--- Vostok.Core/Logging/LogEventExtensions.cs
+++ Vostok.Core/Logging/LogEventExtensions.cs
@@ -14,6 +14,21 @@ namespace Vostok.Logging
                 logEvent.Properties.Union(properties));
         }
 
+        public static LogEvent AddProperty(this LogEvent logEvent, string key, object value)
+        {
+            var properties = new Dictionary<string, object>();
+            foreach (var kvp in logEvent.Properties)
+            {
+                properties.Add(kvp.Key, kvp.Value);
+            }
+            properties.Add(key, value);
+            return new LogEvent(
+                logEvent.Level,
+                logEvent.Exception,
+                logEvent.MessageTemplate,
+                logEvent.MessageParameters,
+                properties);
+        }
         private static IReadOnlyDictionary<string, object> Union(this IReadOnlyDictionary<string, object> first, IReadOnlyDictionary<string, object> second)
         {
             var result = new Dictionary<string, object>();
