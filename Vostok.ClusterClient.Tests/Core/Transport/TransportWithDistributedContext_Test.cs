6e802508d8fe1553c17beefc34d74ad95cfc580b
johneg <johneg@skbkontur.ru>
2017-09-25 15:56:44 +0500

    Add Transport with adding DistributedContext

---
+++ Vostok.ClusterClient.Tests/Core/Transport/TransportWithDistributedContext_Test.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NSubstitute;
+using Vostok.Clusterclient.Model;
+using Vostok.Clusterclient.Transport;
+using Vostok.Flow;
+using Xunit;
+
+namespace Vostok.Clusterclient.Core.Transport
+{
+    public class TransportWithDistributedContext_Test
+    {
+        private readonly ITransport transport;
+        private readonly TransportWithDistributedContext transportWithDistributedContext;
+        private string existedkey;
+
+        public TransportWithDistributedContext_Test()
+        {
+            transport = Substitute.For<ITransport>();
+            transportWithDistributedContext = new TransportWithDistributedContext(transport);
+        }
+
+        [Theory]
+        [InlineData("key", "value", "distributed_context/key", "string%7Cvalue")]
+        [InlineData("ключ", "значение", "distributed_context/%D0%BA%D0%BB%D1%8E%D1%87", "string%7C%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D0%B5")]
+        public void SendAsync_should_create_headers_from_DistributedContext_when_headers_is_null(string key, string value, string expectedKey, string expectedValue)
+        {
+            Context.Configuration.DistributedProperties.Add(key);
+            Context.Properties.SetProperty(key, value);
+
+            var request = new Request("GET", new Uri("http://localhost"));
+            Header[] actual = null;
+            transport.SendAsync(Arg.Do<Request>(x => { actual = x.Headers?.ToArray(); }), Arg.Any<TimeSpan>(), Arg.Any<CancellationToken>()).Returns(new Task<Response>(() => null));
+
+            transportWithDistributedContext.SendAsync(request, TimeSpan.Zero, CancellationToken.None);
+
+            actual.Length.Should().Be(1);
+            actual[0].Name.Should().Be(expectedKey);
+            actual[0].Value.Should().Be(expectedValue);
+        }
+
+        [Fact]
+        public void SendAsync_should_not_create_headers_when_DistributedContext_is_empty_when_headers_is_null()
+        {
+            var request = new Request("GET", new Uri("http://localhost"));
+            Headers actual = null;
+            transport.SendAsync(Arg.Do<Request>(x => { actual = x.Headers; }), Arg.Any<TimeSpan>(), Arg.Any<CancellationToken>()).Returns(new Task<Response>(() => null));
+
+            transportWithDistributedContext.SendAsync(request, TimeSpan.Zero, CancellationToken.None);
+
+            actual.Should().BeNull();
+        }
+
+        [Fact]
+        public void SendAsync_should_add_headers_from_DistributedContext_when_headers_is_not_empty()
+        {
+            const string key1 = "key1";
+            const string key2 = "key2";
+            Context.Configuration.DistributedProperties.Add(key1);
+            Context.Configuration.DistributedProperties.Add(key2);
+            Context.Properties.SetProperty(key1, "value1");
+            Context.Properties.SetProperty(key2, "value2");
+
+            existedkey = "existedKey";
+            var request = new Request("GET", new Uri("http://localhost")).WithHeader(existedkey, "existedValue");
+            Headers actual = null;
+            transport.SendAsync(Arg.Do<Request>(x => { actual = x.Headers; }), Arg.Any<TimeSpan>(), Arg.Any<CancellationToken>()).Returns(new Task<Response>(() => null));
+            var expectedKeys = new[] {existedkey, "distributed_context/" + key1, "distributed_context/" + key2 };
+
+            transportWithDistributedContext.SendAsync(request, TimeSpan.Zero, CancellationToken.None);
+
+            actual.Names.Should().BeEquivalentTo(expectedKeys);
+        }
+    }
+}
---
+++ Vostok.ClusterClient/Transport/TransportWithDistributedContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Vostok.Clusterclient.Model;
+using Vostok.Flow;
+
+namespace Vostok.Clusterclient.Transport
+{
+    public class TransportWithDistributedContext : ITransport
+    {
+        private readonly ITransport transport;
+        public TransportWithDistributedContext(ITransport transport)
+        {
+            this.transport = transport;
+        }
+
+        public Task<Response> SendAsync(Request request, TimeSpan timeout, CancellationToken cancellationToken)
+        {
+            var newRequest = BuildRequestWithDistributedContext(request);
+            return transport.SendAsync(newRequest, timeout, cancellationToken);
+        }
+
+        private Request BuildRequestWithDistributedContext(Request request)
+        {
+            var distributedContext = Context.SerializeDistributedProperties();
+
+            if (distributedContext == null || !distributedContext.Any())
+            {
+                return request;
+            }
+
+            var newHeaders = distributedContext.Select(x => new Header(Encode(MakeHadearKey(x.Key)), Encode(x.Value)));
+            if (request.Headers != null)
+            {
+                newHeaders = newHeaders.Concat(request.Headers);
+            }
+
+            var headersArray = newHeaders.ToArray();
+            return new Request(request.Method, request.Url, request.Content, new Headers(headersArray, headersArray.Length));
+        }
+
+        private static string MakeHadearKey(string key)
+        {
+            return "distributed_context/" + key;
+        }
+
+        private static string Encode(string str)
+        {
+            return Uri.EscapeUriString(str);
+        }
+    }
+}
