662a7ed5e28386777521e66f04ef7edf3eba86bf
iloktionov <iloktionov@skbkontur.ru>
2017-10-02 13:54:46 +0500

    + ConnectTimeoutHelper

---
+++ Vostok.ClusterClient.Tests/Transport.Http/ConnectTimeoutHelper_Tests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Vostok.Clusterclient.Helpers;
+using Vostok.Clusterclient.Transport.Http.Helpers;
+using Vostok.Logging;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Vostok.Clusterclient.Transport
+{
+    public class ConnectTimeoutHelper_Tests
+    {
+        private readonly ILog log;
+
+        public ConnectTimeoutHelper_Tests(ITestOutputHelper outputHelper)
+        {
+            log = new TestOutputLog(outputHelper);
+        }
+
+        [Fact]
+        public void Should_be_able_to_build_socket_check_delegate()
+        {
+            ConnectTimeoutHelper.CanCheckSocket.Should().BeTrue();
+        }
+    }
+}
