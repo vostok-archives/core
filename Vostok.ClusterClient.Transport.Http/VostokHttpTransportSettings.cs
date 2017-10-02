3264076c10a87c7cec2111a047401cbb8f3d5c31
iloktionov <iloktionov@skbkontur.ru>
2017-10-02 13:58:07 +0500

    + VostokHttpTransportSettings

--- Vostok.ClusterClient.Transport.Http/VostokHttpTransportSettings.cs
+++ Vostok.ClusterClient.Transport.Http/VostokHttpTransportSettings.cs
@@ -1,7 +1,11 @@
-﻿namespace Vostok.Clusterclient.Transport.Http
+﻿using System;
+
+namespace Vostok.Clusterclient.Transport.Http
 {
     public class VostokHttpTransportSettings
     {
-        
+        public int ConnectionAttempts { get; set; } = 1;
+
+        public TimeSpan? ConnectionTimeout = TimeSpan.FromMilliseconds(500);
     }
 }
