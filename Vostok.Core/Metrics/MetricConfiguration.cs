40d1acc8c41b551f156ddc12d41301f76e08b590
ezsilmar <zhirov@skbkontur.ru>
2017-10-02 17:32:45 +0500

    default configuration impl

---
+++ Vostok.Core/Metrics/MetricConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Vostok.Airlock;
+using Vostok.Commons.Collections;
+
+namespace Vostok.Metrics
+{
+    public class MetricConfiguration : IMetricConfiguration
+    {
+        public IAirlock Airlock { get; set; }
+        public string EventRoutingKey { get; set; } = "metric_events";
+        public string MetricRoutingKey { get; set; } = "metrics";
+        public ISet<string> ContextFieldsWhitelist { get; } = new ConcurrentSet<string>(StringComparer.Ordinal);
+    }
+}
