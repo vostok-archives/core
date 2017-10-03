1bcccef069d58b2a8a1443518401639f5c24e2f0
iloktionov <iloktionov@skbkontur.ru>
2017-10-03 22:53:06 +0500

    Tracing annotation name constants.

---
+++ Vostok.ClusterClient/Helpers/TracingAnnotations.cs
@@ -0,0 +1,19 @@
+﻿namespace Vostok.Clusterclient.Helpers
+{
+    internal static class TracingAnnotations
+    {
+        public const string OperationName = "operationName";
+        public const string ServiceName = "serviceName";
+        public const string Component = "component";
+        public const string Kind = "kind";
+
+        public const string ClusterStrategy = "cluster.strategy";
+        public const string ClusterStatus = "cluster.status";
+
+        public const string HttpUrl = "http.url";
+        public const string HttpCode = "http.code";
+        public const string HttpMethod = "http.method";
+        public const string HttpRequestContentLength = "http.requestContentLength";
+        public const string HttpResponseContentLength = "http.responseContentLength";
+    }
+}
