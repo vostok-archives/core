7e5fd0a8fe719130c6453cf00c96aae419b58a8d
aa <aa@skbkontur.ru>
2017-10-03 10:58:44 +0500

    Change LogEventData and its serialization

--- Vostok.Core/Logging/LogEventData.cs
+++ Vostok.Core/Logging/LogEventData.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Vostok.Logging
 {
     public sealed class LogEventData
     {
-        public long Timestamp { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
 
-        public IDictionary<string, string> Properties { get; set; }
+        public string LogLevel { get; set; }
+
+        public string MessageTemplate { get; set; }
+
+        public string Exception { get; set; }
+
+        public IReadOnlyDictionary<string, string> Properties { get; set; }
     }
 }
---
+++ Vostok.Core/Logging/LogEventDataSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Vostok.Airlock;
+using Vostok.Commons.Binary;
+
+namespace Vostok.Logging
+{
+    public class LogEventDataSerializer : IAirlockSerializer<LogEventData>, IAirlockDeserializer<LogEventData>
+    {
+        public LogEventData Deserialize(IAirlockSource source)
+        {
+            var reader = source.Reader;
+
+            return new LogEventData
+            {
+                Timestamp = new DateTimeOffset(reader.ReadInt64(), TimeSpan.Zero),
+                LogLevel = reader.ReadString(),
+                MessageTemplate = reader.ReadString(),
+                Exception = reader.ReadString(),
+                Properties = reader.ReadDictionary(r => r.ReadString(), r => r.ReadString())
+            };
+        }
+
+        public void Serialize(LogEventData item, IAirlockSink sink)
+        {
+            var writer = sink.Writer;
+
+            writer.Write(item.Timestamp.UtcTicks);
+            writer.Write(item.LogLevel);
+            writer.Write(item.MessageTemplate);
+            writer.Write(item.Exception);
+            writer.WriteDictionary(item.Properties, (w, s) => w.Write(s), (w, o) => w.Write(o));
+        }
+    }
+}
