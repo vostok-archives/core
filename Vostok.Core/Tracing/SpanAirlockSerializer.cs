d1877ec77653caa1e8c4c6b895d6a7fb4cf17cc0
Andrey Lapin <lapin@kontur.ru>
2017-10-03 16:20:40 +0400

    add Deserialize to SpanAirlockSerializer

--- Vostok.Core/Tracing/SpanAirlockSerializer.cs
+++ Vostok.Core/Tracing/SpanAirlockSerializer.cs
@@ -1,9 +1,11 @@
-﻿using Vostok.Airlock;
+﻿using System;
+using System.IO;
+using Vostok.Airlock;
 using Vostok.Commons.Binary;
 
 namespace Vostok.Tracing
 {
-    internal class SpanAirlockSerializer : IAirlockSerializer<Span>
+    internal class SpanAirlockSerializer : IAirlockSerializer<Span>, IAirlockDeserializer<Span>
     {
         private const byte FormatVersion = 1;
 
@@ -19,6 +21,24 @@ namespace Vostok.Tracing
             writer.Write(span.BeginTimestamp.UtcTicks);
             writer.WriteNullable(span.EndTimestamp, (w, t) => w.Write(t.UtcTicks));
             writer.WriteDictionary(span.Annotations, (w, key) => w.Write(key), (w, value) => w.Write(value));
+        }
+
+        public Span Deserialize(IAirlockSource source)
+        {
+            var reader = source.Reader;
+            var version = reader.ReadByte();
+            if (version != FormatVersion)
+                throw new InvalidDataException("invalid format version: " + version);
+            return new Span
+            {
+                TraceId = reader.ReadGuid(),
+                SpanId = reader.ReadGuid(),
+                ParentSpanId = reader.ReadNullableStruct(x => x.ReadGuid()),
+                OperationName = reader.ReadString(),
+                BeginTimestamp = new DateTimeOffset(reader.ReadInt64(), TimeSpan.Zero),
+                EndTimestamp = reader.ReadNullableStruct(x => new DateTimeOffset(x.ReadInt64(), TimeSpan.Zero)),
+                Annotations = reader.ReadDictionary(r => r.ReadString(), r => r.ReadString())
+            };
         }
     }
 }
