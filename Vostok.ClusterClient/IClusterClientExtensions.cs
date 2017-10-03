801b008895e8d146f9ce183055b87d1e837522a1
iloktionov <iloktionov@skbkontur.ru>
2017-10-03 22:57:27 +0500

    Added extensions which accept operation names and put them in context.

--- Vostok.ClusterClient/IClusterClientExtensions.cs
+++ Vostok.ClusterClient/IClusterClientExtensions.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
+using Vostok.Clusterclient.Helpers;
 using Vostok.Clusterclient.Model;
 using Vostok.Clusterclient.Strategies;
 using Vostok.Commons.Model;
+using Vostok.Flow;
 
 namespace Vostok.Clusterclient
 {
     public static class IClusterClientExtensions
     {
+        /// <summary>
+        /// <para>Sends given request using given <paramref name="timeout"/>, <paramref name="strategy"/> and <paramref name="cancellationToken"/>.</para>
+        /// <para>Uses <see cref="IClusterClientConfiguration.DefaultTimeout"/> if provided <paramref name="timeout"/> is <c>null</c>.</para>
+        /// <para>Uses <see cref="IClusterClientConfiguration.DefaultRequestStrategy"/> if provided <paramref name="strategy"/> is <c>null</c>.</para>
+        /// <para>See <see cref="IRequestStrategy.SendAsync"/> for more details about what a request strategy is.</para>
+        /// <para>See <see cref="Strategy"/> class for some prebuilt strategies and convenient factory methods.</para>
+        /// </summary>
+        [NotNull]
+        public static async Task<ClusterResult> SendAsync(
+            [NotNull] this IClusterClient client,
+            [NotNull] Request request,
+            [CanBeNull] TimeSpan? timeout = null,
+            [CanBeNull] IRequestStrategy strategy = null,
+            CancellationToken cancellationToken = default(CancellationToken),
+            [CanBeNull] RequestPriority? priority = null,
+            [CanBeNull] string operationName = null)
+        {
+            using (operationName == null ? null : Context.Properties.Use(TracingAnnotations.OperationName, operationName))
+            {
+                return await client.SendAsync(request, timeout, strategy, cancellationToken, priority).ConfigureAwait(false);
+            }
+        }
+
         /// <summary>
         /// <para>Sends given request using given <paramref name="timeout"/>, <paramref name="strategy"/> and <paramref name="cancellationToken"/>.</para>
         /// <para>Uses <see cref="IClusterClientConfiguration.DefaultTimeout"/> if provided <paramref name="timeout"/> is <c>null</c>.</para>
@@ -23,9 +49,13 @@ namespace Vostok.Clusterclient
             [CanBeNull] TimeSpan? timeout = null,
             [CanBeNull] IRequestStrategy strategy = null,
             CancellationToken cancellationToken = default(CancellationToken),
-            [CanBeNull] RequestPriority? priority = null)
+            [CanBeNull] RequestPriority? priority = null,
+            [CanBeNull] string operationName = null)
         {
-            return client.SendAsync(request, timeout, strategy, cancellationToken).GetAwaiter().GetResult();
+            using (operationName == null ? null : Context.Properties.Use(TracingAnnotations.OperationName, operationName))
+            {
+                return client.SendAsync(request, timeout, strategy, cancellationToken, priority).GetAwaiter().GetResult();
+            }
         }
     }
 }
