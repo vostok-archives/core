fb39f23a6392c2cb4d781370d9fdf5ccf9cca58f
iloktionov <iloktionov@skbkontur.ru>
2017-10-03 10:45:09 +0500

    First functional test for transport.

---
+++ Vostok.ClusterClient.Tests/Transport.Http/TransportTestsBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using Vostok.Clusterclient.Helpers;
+using Vostok.Clusterclient.Model;
+using Vostok.Logging;
+using Xunit.Abstractions;
+
+namespace Vostok.Clusterclient.Transport.Http
+{
+    public class TransportTestsBase
+    {
+        protected readonly VostokHttpTransport transport;
+        protected readonly ILog log;
+
+        public TransportTestsBase(ITestOutputHelper outputHelper)
+        {
+            log = new TestOutputLog(outputHelper);
+            transport = new VostokHttpTransport(log);
+        }
+
+        protected Response Send(Request request, TimeSpan? timeout = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return transport.SendAsync(request, timeout ?? TimeSpan.FromMinutes(1), cancellationToken).GetAwaiter().GetResult();
+        }
+    }
+}
