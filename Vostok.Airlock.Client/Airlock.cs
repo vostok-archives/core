b99ac293621c38c60ae2a7a20a43284e7fb2ef90
iloktionov <iloktionov@skbkontur.ru>
2017-10-04 11:47:37 +0500

    Added a stub of Airlock implementation.

---
+++ Vostok.Airlock.Client/Airlock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vostok.Airlock
+{
+    public class Airlock : IAirlock
+    {
+        private readonly AirlockConfig config;
+
+        public Airlock(AirlockConfig config)
+        {
+            this.config = config ?? throw new ArgumentNullException(nameof(config));
+        }
+
+        public void Push<T>(string routingKey, T item)
+        {
+            if (!AirlockSerializerRegistry.TryGet<T>(out var serializer))
+                return;
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vostok.Airlock.Client/AirlockConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Vostok.Commons.Extensions.UnitConvertions;
+using Vostok.Commons.Model;
+
+namespace Vostok.Airlock
+{
+    public class AirlockConfig
+    {
+        public string ApiKey = "insert api key here";
+
+        public DataSize MaximumMemoryConsumption { get; set; } = 128.Megabytes();
+
+        public DataSize MaximumBatchSizeToSend { get; set; } = 4.Megabytes();
+
+        public DataSize InitialPooledBufferSize { get; set; } = 32.Kilobytes();
+
+        public int InitialPooledBuffersCount { get; set; } = 16;
+
+        public TimeSpan SendPeriod { get; set; } = 5.Seconds();
+
+        public TimeSpan SendPeriodCap { get; set; } = 5.Minutes();
+
+        public TimeSpan RequestTimeout { get; set; } = 20.Seconds();
+    }
+}
