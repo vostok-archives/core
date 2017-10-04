3473250a350fa65a309f955c7299f25dbf12594d
iloktionov <iloktionov@skbkontur.ru>
2017-10-04 13:35:07 +0500

    Implemented record serialization.

---
+++ Vostok.Airlock.Client.Tests/RequestSender_Tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Vostok.Commons.Binary;
+
+namespace Vostok.Airlock
+{
+    [TestFixture]
+    internal class RecordSerializer_Tests
+    {
+        private const int BufferSize = 32;
+
+        private Buffer buffer;
+        private MemoryManager manager;
+        private RecordSerializer serializer;
+
+        private DateTimeOffset timestamp;
+        private string item;
+        private IAirlockSerializer<string> itemSerializer;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            manager = new MemoryManager(BufferSize*2, BufferSize);
+            buffer = new Buffer(new BinaryBufferWriter(BufferSize), manager);
+            serializer = new RecordSerializer();
+
+            timestamp = DateTimeOffset.Now;
+            item = "Hello!";
+            itemSerializer = Substitute.For<IAirlockSerializer<string>>();
+            itemSerializer
+                .WhenForAnyArgs(s => s.Serialize(null, null))
+                .Do(info => info.Arg<IAirlockSink>().Writer.Write(info.Arg<string>()));
+        }
+
+        [Test]
+        public void TrySerialize_should_correctly_serialize_record_given_sufficient_memory()
+        {
+            serializer.TrySerialize(item, itemSerializer, timestamp, buffer).Should().BeTrue();
+
+            var reader = new BinaryBufferReader(buffer.InternalBuffer, 0);
+
+            reader.ReadInt64().Should().Be(timestamp.ToUniversalTime().ToUnixTimeMilliseconds());
+            reader.ReadInt32().Should().Be(10);
+            reader.ReadString().Should().Be(item);
+        }
+
+        [Test]
+        public void TrySerialize_should_return_false_when_available_memory_is_insufficient()
+        {
+            item = new string('!', BufferSize*4);
+
+            serializer.TrySerialize(item, itemSerializer, timestamp, buffer).Should().BeFalse();
+        }
+    }
+}
---
+++ Vostok.Airlock.Client/RecordSerializer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Vostok.Airlock
+{
+    internal class RecordSerializer : IRecordSerializer
+    {
+        public bool TrySerialize<T>(T item, IAirlockSerializer<T> serializer, DateTimeOffset timestamp, IBuffer buffer)
+        {
+            try
+            {
+                var unixTimestamp = timestamp.ToUniversalTime().ToUnixTimeMilliseconds();
+                var writer = buffer.Writer;
+
+                writer.Write(unixTimestamp);
+
+                var payloadLengthPosition = writer.Position;
+
+                writer.Write(0);
+
+                var positionBeforeSerialization = writer.Position;
+
+                serializer.Serialize(item, buffer);
+
+                var positionAfterSerialization = writer.Position;
+
+                writer.Position = payloadLengthPosition;
+
+                writer.Write(positionAfterSerialization - positionBeforeSerialization);
+
+                writer.Position = positionAfterSerialization;
+
+                return true;
+            }
+            catch (InternalBufferOverflowException)
+            {
+                return false;
+            }
+        }
+    }
+}
