5b532fedea5b2e1396ed84c8724d9f7f9655762f
iloktionov <iloktionov@skbkontur.ru>
2017-10-04 14:33:25 +0500

    IAirlock.Push(): nullable timestamp.

--- Vostok.Core/Airlock/IAirlock.cs
+++ Vostok.Core/Airlock/IAirlock.cs
@@ -4,6 +4,6 @@ namespace Vostok.Airlock
 {
     public interface IAirlock
     {
-        void Push<T>(string routingKey, T item, DateTimeOffset timestamp = default(DateTimeOffset));
+        void Push<T>(string routingKey, T item, DateTimeOffset? timestamp = null);
     }
 }
