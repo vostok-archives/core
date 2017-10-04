eacdeb73be4a6211e9ab344c42316d9d3322f989
aa <aa@skbkontur.ru>
2017-10-05 00:29:44 +0500

    Add minLevel to LogWithPropertyFilter

--- Vostok.Core/Logging/ILogExtensions_Filters.cs
+++ Vostok.Core/Logging/ILogExtensions_Filters.cs
@@ -4,7 +4,7 @@ namespace Vostok.Logging
 {
     public static class ILogExtensions_Filters
     {
-        public static ILog FilterByLevel(this ILog log, LogLevel minLevel)
+        public static ILog FilterByLevel(this ILog log, LogLevel minLevel = LogLevel.Debug)
         {
             if (log == null)
                 throw new ArgumentNullException(nameof(log));
@@ -12,7 +12,7 @@ namespace Vostok.Logging
             return new LogWithLevel(log, minLevel);
         }
 
-        public static ILog FilterByProperty<T>(this ILog log, string propertyName, Func<T, bool> propertyFilter)
+        public static ILog FilterByProperty<T>(this ILog log, string propertyName, Func<T, bool> propertyFilter, LogLevel minLevel = LogLevel.Debug)
         {
             if (log == null)
                 throw new ArgumentNullException(nameof(log));
@@ -23,7 +23,7 @@ namespace Vostok.Logging
             if (propertyFilter == null)
                 throw new ArgumentNullException(nameof(propertyFilter));
 
-            return new LogWithPropertyFilter<T>(log, propertyName, propertyFilter);
+            return new LogWithPropertyFilter<T>(log, propertyName, propertyFilter, minLevel);
         }
 
         private class LogWithLevel : ILog
@@ -56,19 +56,22 @@ namespace Vostok.Logging
             private readonly ILog log;
             private readonly string propertyName;
             private readonly Func<TProperty, bool> propertyFilter;
+            private readonly LogLevel minLevel;
 
-            public LogWithPropertyFilter(ILog log, string propertyName, Func<TProperty, bool> propertyFilter)
+            public LogWithPropertyFilter(ILog log, string propertyName, Func<TProperty, bool> propertyFilter, LogLevel minLevel)
             {
                 this.log = log;
                 this.propertyName = propertyName;
                 this.propertyFilter = propertyFilter;
+                this.minLevel = minLevel;
             }
 
             public void Log(LogEvent logEvent)
             {
                 if (logEvent.Properties.TryGetValue(propertyName, out var propertyValue)
                     && propertyValue is TProperty property
-                    && propertyFilter.Invoke(property))
+                    && propertyFilter.Invoke(property)
+                    && logEvent.Level < minLevel)
                     return;
 
                 log.Log(logEvent);
