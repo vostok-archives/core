73a40d4d83e1dbe5507262dc70b7dcaa769e6e2d
iloktionov <iloktionov@skbkontur.ru>
2017-10-04 13:52:10 +0500

    Airlock: implemented Push() method.

--- Vostok.Airlock.Client/Airlock.cs
+++ Vostok.Airlock.Client/Airlock.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 namespace Vostok.Airlock
 {
     public class Airlock : IAirlock
     {
         private readonly AirlockConfig config;
+        private readonly MemoryManager memoryManager;
+        private readonly RecordWriter recordWriter;
+        private readonly ConcurrentDictionary<string, BufferPool> bufferPools;
 
         public Airlock(AirlockConfig config)
         {
             this.config = config ?? throw new ArgumentNullException(nameof(config));
+
+            memoryManager = new MemoryManager(config.MaximumMemoryConsumption.Bytes, (int) config.InitialPooledBufferSize.Bytes);
+            recordWriter = new RecordWriter(new RecordSerializer());
+            bufferPools = new ConcurrentDictionary<string, BufferPool>();
         }
 
         public void Push<T>(string routingKey, T item)
@@ -16,7 +24,12 @@ namespace Vostok.Airlock
             if (!AirlockSerializerRegistry.TryGet<T>(out var serializer))
                 return;
 
-            throw new NotImplementedException();
+            recordWriter.Write(item, serializer, DateTimeOffset.UtcNow, ObtainBufferPool(routingKey));
+        }
+
+        private BufferPool ObtainBufferPool(string routingKey)
+        {
+            return bufferPools.GetOrAdd(routingKey, _ => new BufferPool(memoryManager, config.InitialPooledBuffersCount));
         }
     }
 }
