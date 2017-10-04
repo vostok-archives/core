1b06eaa7eaca7b39b94724b0118a035cdfb777d9
iloktionov <iloktionov@skbkontur.ru>
2017-10-04 10:03:54 +0500

    AirlockSerializerRegistry.Register() now requires data type parameter.

--- Vostok.Core/Airlock/AirlockSerializerRegistry.cs
+++ Vostok.Core/Airlock/AirlockSerializerRegistry.cs
@@ -1,21 +1,33 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 
 namespace Vostok.Airlock
 {
     public static class AirlockSerializerRegistry
     {
-        public static void Register<T>(IAirlockSerializer<T> serializer)
+        public static void Register<T>(string dataType, IAirlockSerializer<T> serializer)
         {
+            if (dataType == null)
+                throw new ArgumentNullException(nameof(dataType));
+
+            if (serializer == null)
+                throw new ArgumentNullException(nameof(serializer));
+
+            Interlocked.Exchange(ref Container<T>.DataType, dataType);
             Interlocked.Exchange(ref Container<T>.Serializer, serializer);
         }
 
-        public static bool TryGet<T>(out IAirlockSerializer<T> serializer)
+        public static bool TryGet<T>(out string dataType, out IAirlockSerializer<T> serializer)
         {
-            return (serializer = Container<T>.Serializer) != null;
+            dataType = Container<T>.DataType;
+            serializer = Container<T>.Serializer;
+
+            return dataType != null && serializer != null;
         }
 
         private static class Container<T>
         {
+            public static string DataType;
             public static IAirlockSerializer<T> Serializer;
         }
     }
--- Vostok.Core/Tracing/Trace.cs
+++ Vostok.Core/Tracing/Trace.cs
@@ -19,7 +19,7 @@ namespace Vostok.Tracing
                     Annotations = new Dictionary<string, string>()
                 });
 
-            AirlockSerializerRegistry.Register(new SpanAirlockSerializer());
+            AirlockSerializerRegistry.Register("traces", new SpanAirlockSerializer());
         }
 
         public static ISpanBuilder BeginSpan()
