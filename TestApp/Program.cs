cbb75807980cdaa85725d1a710d63e68a8271ccd
iloktionov <iloktionov@skbkontur.ru>
2017-10-05 23:02:42 +0500

    TestApp now uses ParallelAirlockClient.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,34 +1,37 @@
 ﻿using System;
 using System.Text;
-using System.Threading;
 using Vostok.Airlock;
 using Vostok.Clusterclient.Topology;
-using Vostok.Commons.Binary;
 using Vostok.Commons.Extensions.UnitConvertions;
+using Vostok.Commons.Threading;
 using Vostok.Logging.Logs;
 
 namespace TestApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             AirlockSerializerRegistry.Register(new Serializer());
 
+            ThreadPoolUtility.Setup(new SilentLog());
+
             var config = new AirlockConfig
             {
+                ClusterProvider = new FixedClusterProvider(
+                    new Uri("http://vostok04:8888/"), 
+                    new Uri("http://vostok06:8888/")),
                 ApiKey = "UniversalApiKey",
                 SendPeriod = 1.Seconds(),
                 SendPeriodCap = 30.Seconds(),
             };
 
-            var airlock = new AirlockClient(config, new ConsoleLog());
+            var airlock = new ParallelAirlockClient(config, 8, new ConsoleLog());
+            var item = Guid.NewGuid().ToString();
 
             while (true)
             {
-                Thread.Sleep(250);
-
-                airlock.Push("iloktionov-test", Guid.NewGuid().ToString());
+                airlock.Push("iloktionov-test", item);
             }
         }
 
