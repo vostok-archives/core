ab4848e29599bc3363bbd2f308d87501329f17d6
ezsilmar <zhirov@skbkontur.ru>
2017-10-05 17:15:16 +0500

    Added flush async method

--- Vostok.Airlock.Client/AirlockClient.cs
+++ Vostok.Airlock.Client/AirlockClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Threading.Tasks;
 using Vostok.Commons.Synchronization;
 using Vostok.Logging;
 using Vostok.Logging.Logs;
@@ -50,6 +51,11 @@ namespace Vostok.Airlock
             {
                 lostItemsCounter.Increment();
             }
+        }
+
+        public Task FlushAsync()
+        {
+            return dataSenderDaemon.FlushAsync();
         }
 
         public void Dispose()
