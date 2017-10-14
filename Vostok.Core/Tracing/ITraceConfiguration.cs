53d33f8a136b928665fce31c9b49e834c190980c
spaceorc <spaceorc@skbkontur.ru>
2017-10-15 00:53:19 +0500

    Add some TODOs

--- Vostok.Core/Tracing/ITraceConfiguration.cs
+++ Vostok.Core/Tracing/ITraceConfiguration.cs
@@ -5,8 +5,14 @@ namespace Vostok.Tracing
 {
     public interface ITraceConfiguration
     {
+        /// <summary>
+        /// TODO create xmldoc for this
+        /// </summary>
         ISet<string> ContextFieldsWhitelist { get; }
 
+        /// <summary>
+        /// TODO create xmldoc for this
+        /// </summary>
         ISet<string> InheritedFieldsWhitelist { get; }
 
         Func<bool> IsEnabled { get; set; }
