6bb20618babcc2328e2c7b742f53006d64c53404
spaceorc <spaceorc@skbkontur.ru>
2017-10-21 00:31:16 +0500

    Add DurationParser and tests for many other parsers

---
+++ Vostok.Core.Tests/Common/DoubleParser_Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Vostok.Commons.Utilities;
+
+namespace Vostok.Common
+{
+    [TestFixture]
+    internal class DoubleParser_Tests
+    {
+        [TestCase("0", 0D)]
+        [TestCase("1", 1D)]
+        [TestCase("0.345", 0.345D)]
+        [TestCase("0,345", 0.345D)]
+        public void Should_parse_correct_value_from_given_input(string input, double expected)
+        {
+            Assert.AreEqual(expected, DoubleParser.ParseDouble(input));
+        }
+    }
+}
