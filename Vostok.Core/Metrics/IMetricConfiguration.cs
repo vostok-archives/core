cc4a81df9137909ba753c9e927de0ac60737ae64
spaceorc <spaceorc@skbkontur.ru>
2017-10-28 00:02:32 +0500

    Add some xmldocs

--- Vostok.Core/Metrics/IMetricConfiguration.cs
+++ Vostok.Core/Metrics/IMetricConfiguration.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.Generic;
+using Vostok.Flow;
 
 namespace Vostok.Metrics
 {
     public interface IMetricConfiguration
     {
-        IMetricEventReporter Reporter { get; set; }
+        /// <summary>
+        /// Fields to be added as tags from current <see cref="Context"/>
+        /// </summary>
         ISet<string> ContextFieldsWhitelist { get; }
+
+        IMetricEventReporter Reporter { get; set; }
     }
 }
