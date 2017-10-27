590f43995270f5350b703e4165d41d76529f8347
spaceorc <spaceorc@skbkontur.ru>
2017-10-27 23:41:31 +0500

    Add some xmldocs

--- Vostok.Core/Tracing/ITraceConfiguration.cs
+++ Vostok.Core/Tracing/ITraceConfiguration.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using Vostok.Flow;
 
 namespace Vostok.Tracing
 {
     public interface ITraceConfiguration
     {
+        /// <summary>
+        /// Fields to be added as trace annotations from current <see cref="Context"/>
+        /// </summary>
         ISet<string> ContextFieldsWhitelist { get; }
 
+        /// <summary>
+        /// Fields to be added as trace annotations from parent span
+        /// </summary>
         ISet<string> InheritedFieldsWhitelist { get; }
 
         Func<bool> IsEnabled { get; set; }
