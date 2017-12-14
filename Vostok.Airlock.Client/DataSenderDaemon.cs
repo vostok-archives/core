ba80ec9ef6033a13b9cc95384ffdaf2c6e96ec03
ezsilmar <zhirov@skbkontur.ru>
2017-12-14 18:18:50 +0500

    Debug output everywhere

--- Vostok.Airlock.Client/DataSenderDaemon.cs
+++ Vostok.Airlock.Client/DataSenderDaemon.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Vostok.Commons.Synchronization;
 using Vostok.Logging;
@@ -18,6 +19,7 @@ namespace Vostok.Airlock
         private readonly AtomicInt currentState;
 
         private volatile IterationHandle currentIteration;
+        private readonly Guid id = Guid.NewGuid();
 
         public DataSenderDaemon(IDataSender dataSender, AirlockConfig config, ILog log)
         {
@@ -31,58 +33,80 @@ namespace Vostok.Airlock
 
         public void Start()
         {
+            log.Warn($"{id:N} Start. Started.");
             if (currentState.TrySet(stateStarted, stateNotStarted))
             {
+                log.Warn($"{id:N} Start. Passed start check.");
                 Task.Run(SendingRoutine);
+                log.Warn($"{id:N} Start. Started routine.");
             }
+            log.Warn($"{id:N} Start. Finished");
         }
 
         public async Task FlushAsync()
         {
+            log.Warn($"{id:N} Flush. Started");
             if (currentState.Value == stateStarted)
             {
                 var iteration = currentIteration;
                 if (iteration == null)
                 {
+                    log.Warn($"{id:N} Flush. Finished. Iteration was null");
                     return;
                 }
-
+                
                 iteration.WakeUp();
+                log.Warn($"{id:N} Flush. Waked up current iteration");
                 var nextIteration = await iteration.WaitIterationFinished();
+                log.Warn($"{id:N} Flush. Current iteration wait finished.");
                 if (nextIteration == null)
                 {
+                    log.Warn($"{id:N} Flush. Finished. Next iteration was null");
                     return;
                 }
                 
                 await nextIteration.WaitSendFinished();
+                log.Warn($"{id:N} Flush. Finished. Next iteration send finished.");
             }
         }
 
         public void Dispose()
         {
+            log.Warn($"{id:N} Dispose. Started.");
             FlushAsync().GetAwaiter().GetResult();
+            log.Warn($"{id:N} Dispose. Flush finished.");
             currentState.Value = stateDisposed;
-            currentIteration?.WakeUp();
+            log.Warn($"{id:N} Dispose. Set state to disposed");
+            currentIteration?.WakeUp(); // Вот здесь потрачено - можем прождать целый период отправки.
+            log.Warn($"{id:N} Dispose. Finished. Waked up current iteration");
         }
 
         private async Task SendingRoutine()
         {
+            log.Warn($"{id:N} Routine. Started");
             var sendPeriod = config.SendPeriod;
 
             while (currentState.Value != stateDisposed)
             {
+                log.Warn($"{id:N} Routine. Passed dispose check");
                 ReportNextIteration(new IterationHandle());
+                log.Warn($"{id:N} Routine. Next iteration reported");
 
                 var (result, sendTime) = await SendAsync().ConfigureAwait(false);
+                log.Warn($"{id:N} Routine. Send finished");
 
                 AdjustSendPeriod(result, ref sendPeriod);
 
                 currentIteration.ScheduleWakeUp(sendPeriod - sendTime);
 
+                log.Warn($"{id:N} Routine. Going to sleep.");
                 await currentIteration.WaitForNextIteration().ConfigureAwait(false);
+                log.Warn($"{id:N} Routine. Waked up for next iteration");
             }
 
+            log.Warn($"{id:N} Routine. Failed dispose check.");
             ReportNextIteration(null);
+            log.Warn($"{id:N} Routine. Finished. Reported null to current iteration");
         }
 
         private async Task<(DataSendResult, TimeSpan)> SendAsync()
@@ -96,7 +120,7 @@ namespace Vostok.Airlock
             }
             catch (Exception ex)
             {
-                log.Warn("Send failed with exception", ex);
+                log.Warn($"{id:N} Send failed with exception", ex);
                 result = DataSendResult.Ok;
             }
 
