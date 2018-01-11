6fdd3175a83d1f33c04bd637a5bab7adbd7a7b17
lapin <lapin@kontur.ru>
2018-01-11 16:32:34 +0400

    simplify AirlockMetricReporter

--- Vostok.Airlock.Client/Metrics/AirlockMetricReporter.cs
+++ Vostok.Airlock.Client/Metrics/AirlockMetricReporter.cs
@@ -1,30 +1,21 @@
-﻿using System;
-using Vostok.Metrics;
+﻿using Vostok.Metrics;
 
 namespace Vostok.Airlock.Metrics
 {
     public class AirlockMetricReporter : IMetricEventReporter
     {
-        private readonly Func<IAirlockClient> getAirlockClient;
-        private readonly Func<string> getMetricsRoutingKey;
-        private readonly Func<string> getAppEventsRoutingKey;
+        private readonly IAirlockClient airlockClient;
+        private readonly string routingKeyPrefix;
 
         public AirlockMetricReporter(IAirlockClient airlockClient, string routingKeyPrefix)
-            : this(() => airlockClient, () => routingKeyPrefix)
         {
-        }
-
-        public AirlockMetricReporter(Func<IAirlockClient> getAirlockClient, Func<string> routingKeyPrefix)
-        {
-            this.getAirlockClient = getAirlockClient;
-            getAppEventsRoutingKey = () => RoutingKey.TryAddSuffix(routingKeyPrefix(), RoutingKey.AppEventsSuffix);
-            getMetricsRoutingKey = () => RoutingKey.TryAddSuffix(routingKeyPrefix(), RoutingKey.MetricsSuffix);
+            this.airlockClient = airlockClient;
+            this.routingKeyPrefix = routingKeyPrefix;
         }
 
         public void SendEvent(MetricEvent metricEvent)
         {
-            var airlockClient = getAirlockClient();
-            var routingKey = getAppEventsRoutingKey();
+            var routingKey = RoutingKey.TryAddSuffix(routingKeyPrefix, RoutingKey.AppEventsSuffix);
             if (airlockClient == null || string.IsNullOrEmpty(routingKey))
                 return;
             airlockClient.Push(routingKey, metricEvent, metricEvent.Timestamp);
@@ -32,8 +23,7 @@ namespace Vostok.Airlock.Metrics
 
         public void SendMetric(MetricEvent metricEvent)
         {
-            var airlockClient = getAirlockClient();
-            var routingKey = getMetricsRoutingKey();
+            var routingKey = RoutingKey.TryAddSuffix(routingKeyPrefix, RoutingKey.MetricsSuffix);
             if (airlockClient == null || string.IsNullOrEmpty(routingKey))
                 return;
             airlockClient.Push(routingKey, metricEvent, metricEvent.Timestamp);
