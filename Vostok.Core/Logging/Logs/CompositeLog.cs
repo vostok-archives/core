d33736d849c2ca0340b2d345939f41acb4ddc8e1
spaceorc <spaceorc@skbkontur.ru>
2018-02-15 16:35:22 +0500

    Add CompositeLog

---
+++ Vostok.Core/Logging/Logs/CompositeLog.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace Vostok.Logging.Logs
+{
+    public class CompositeLog : ILog
+    {
+        private readonly ILog[] logs;
+
+        public CompositeLog(params ILog[] logs)
+        {
+            this.logs = logs;
+        }
+
+        public void Log(LogEvent logEvent)
+        {
+            foreach (var log in logs)
+                log.Log(logEvent);
+        }
+
+        public bool IsEnabledFor(LogLevel level)
+        {
+            return logs.Any(l => l.IsEnabledFor(level));
+        }
+    }
+}
